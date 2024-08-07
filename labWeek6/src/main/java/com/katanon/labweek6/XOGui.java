cc90b481f4e2006631b24d71972bfb1c08394365
KTN-katanon <66160196@go.buu.ac.th>
2024-08-07 21:24:25 +0700

    Create Project

---
+++ labWeek6/src/main/java/com/katanon/labweek6/XOGui.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.katanon.labweek6;
+
+/**
+ *
+ * @author nonku
+ */
+public class XOGui {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
